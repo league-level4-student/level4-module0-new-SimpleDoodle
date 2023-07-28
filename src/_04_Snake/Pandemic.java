1bcab6815cf86b4cb02a31c0a63535275df84019
jereycui2008 <jeremycuid@gmail.com>
2023-07-27 18:33:12 -0700

    Peasant Railgun

---
+++ src/_04_Snake/Pandemic.java
@@ -0,0 +1,36 @@
+package _04_Snake;
+
+import java.util.*;
+
+public class Pandemic {
+
+	public static double infected(String input) {
+		ArrayList<String> inputList = new ArrayList<String>();
+		ArrayList<String> sortedList = new ArrayList<String>();
+		StringBuilder builder = new StringBuilder();
+		ArrayList<Integer> counter = new ArrayList<Integer>();
+		builder.append(input);
+		inputList.add(input);
+		counter.add(0);
+		if (input.contains("X")) {
+			for (int i = 0; i < input.length(); i++) {
+				if (inputList.get(i).equals("X")) {
+					counter.add(i);
+				}
+			}
+			counter.add(input.length());
+				for (int j = 0; j < counter.size() + 1; j++) {
+					inputList.subList(counter.get(j), counter.get(j+1));
+				}
+			
+		} else {
+			if (input.contains("1")) {
+				return 100;
+			} else {
+				return 0;
+			}
+
+		}
+		return 0;
+	}
+}
